3643d133742aa1bbea90544ed0be358014b125b8
Arina <ari3488@yandex.ru>
2020-12-22 00:46:45 +0300

    adaptive header

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-// (function($){
-// 	$('.menu-bar').on('click', function() {
-// 		$('header').toggleClass('triggered');
-//     });
-// })(jQuery);
-
+$(document).ready(function(){
+    $('.header__burger').click(function(event) {
+        $('.header__burger, .header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
